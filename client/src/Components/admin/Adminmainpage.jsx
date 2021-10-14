d3d83297cb18033c8b4259771f133b49f4e2fe82
ArhamKhan <arhamkhancs99@gmail.com>
2021-10-14 21:59:11 +0500

    modif ying adminPage

--- client/src/Components/admin/Adminmainpage.jsx
+++ client/src/Components/admin/Adminmainpage.jsx
@@ -6,42 +6,35 @@ const Adminmainpage = () => {
         <>
         <div class="container">
     <div class="row">
-        <div class="col-md-4 col-xl-3">
-            <div class="card bg-c-blue order-card">
-                <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-cart-plus f-left"></i><span>486</span></h2>
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-md-4 col-xl-3">
+        <div class="col-md-4 col-xl-4">
             <div class="card bg-c-green order-card">
                 <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-rocket f-left"></i><span>486</span></h2>
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
+                    <h6 class="m-b-20" style={{fontSize:"18px"}}>All Orders</h6>
+                    <h2 class="text-right"><i class="fa fa-cart-plus f-left"></i><span>486</span></h2>
+                    <p class="m-b-0" style={{fontSize:"16px"}}>Current Orders<span class="f-right">0</span></p>
                 </div>
             </div>
         </div>
         
-        <div class="col-md-4 col-xl-3">
-            <div class="card bg-c-yellow order-card">
+        <div class="col-md-4 col-xl-4">
+            <div class="card bg-c-blue order-card">
                 <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-refresh f-left"></i><span>486</span></h2>
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
+                    <h6 class="m-b-20" style={{fontSize:"18px"}}>Orders Received</h6>
+                    <h2 class="text-right"><i class="fa fa-rocket f-left"></i><span>486</span></h2>
+                    <p class="m-b-0" style={{fontSize:"14px"}}>Completed Orders<span class="f-right">351</span></p>
                 </div>
             </div>
         </div>
         
-        <div class="col-md-4 col-xl-3">
+     
+        
+        <div class="col-md-4 col-xl-4">
             <div class="card bg-c-pink order-card">
                 <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
+                    <h6 class="m-b-20" style={{fontSize:"18px"}}>Canceled Orders</h6>
                     <h2 class="text-right"><i class="fa fa-credit-card f-left"></i><span>486</span></h2>
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
+                    <p class="m-b-0" style={{fontSize:"14px"}}>pending orders<span class="f-right">351</span></p>
+                    
                 </div>
             </div>
         </div>
